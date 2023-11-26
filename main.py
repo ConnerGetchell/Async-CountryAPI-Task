32dd7acb0fdf0b5747af73f90b6d64b5ece95b0d
Conner Getchell <42593899+ConnerGetchell@users.noreply.github.com>
2023-11-26 15:27:59 -0600

    Doc strings, readability, error catching, logging updates

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 # Standard library imports
 import sys
-import asyncio
+import os
 import logging
+import asyncio
 import csv
+
 # Third-party imports
 import aiohttp
 
-# Change the event loop policy to WindowsSelectorEventLoopPolicy for Windows
+# Change the event loop policy to WindowsSelectorEventLoopPolicy for Windows.
 # See README.md for more details
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-# Set logging level to INFO
-logging.basicConfig(level=logging.INFO)
-
+# Set logging level to INFO for standard output.
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), stream=sys.stdout)
 
 
 async def fetch_country_info(session, country):
+    """
+    Asynchronously fetches data for a single country.
+    Returns: a dictionary of country data.
+    """
     url = f'https://restcountries.com/v3.1/name/{country}?fullText=true'
     try:
         async with session.get(url) as response:
+            if response.status != 200:
+                error_data = await response.json()
+                logging.error(f"Error fetching data for {country}: {error_data['message']}, status code: {response.status}")
+                return None
             data = await response.json()
             return data[0]
     except Exception as e:
@@ -28,30 +37,40 @@ async def fetch_country_info(session, country):
 
 
 async def get_countries_info(countries):
+    """
+    Asynchronously fetches data for all countries in list.
+    Returns: a list of country data dictionaries.
+    """
+    logging.info(f"Fetched data for {countries}")
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_country_info(session, country) for country in countries]
         return await asyncio.gather(*tasks)
 
-# Dynamically find the first currency based on the currency code, returns empty string if no currency is found
-def get_first_currency(country_data):
-    if 'currencies' in country_data and country_data['currencies']:
-        currency_code = next(iter(country_data['currencies']))
-        return country_data['currencies'][currency_code]['name']
-    return ''
 
 def write_to_csv(data):
-    with open('countries_info.csv', 'w', newline='') as csvfile:
-        fieldnames = ['country_name', 'currency', 'capital', 'alt_spellings']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+    """Writes countries data to CSV file."""
+    filename = 'countries_info.csv'
+    try:
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = ['country_name', 'currency', 'capital', 'alt_spellings']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
 
-        for country_data in data:
-            writer.writerow({
-                'country_name': country_data['name']['common'],
-                'currency': get_first_currency(country_data),
-                'capital': country_data['capital'][0] if 'capital' in country_data else '',
-                'alt_spellings': ', '.join(country_data['altSpellings']) if 'altSpellings' in country_data else ''
-            })
+            for country_data in data:
+                writer.writerow({
+                    'country_name': country_data['name']['common'] 
+                        if 'name' in country_data else '',
+                    'currency': next(iter(country_data['currencies'].values()), {}).get('name', '') 
+                        if 'currencies' in country_data else '',
+                    'capital': country_data['capital'][0] 
+                        if 'capital' in country_data else '',
+                    'alt_spellings': ', '.join(country_data['altSpellings']) 
+                        if 'altSpellings' in country_data else ''
+                })
+
+        logging.info(f"Succesfully wrote to CSV: {filename}")
+    except Exception as e:
+        logging.error(f"Error writing to CSV: {e}")
 
 
 async def main():
